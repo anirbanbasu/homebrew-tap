c7c5a7df79e534f10ba5c551cec8522c4fdcb458
Anirban Basu <anirbanbasu@users.noreply.github.com>
2026-02-20 07:42:22 +0900

    fix: Trying again to please the Homebrew/Ruby formatters!
    Signed-off-by: Anirban Basu <anirbanbasu@users.noreply.github.com>

--- Formula/ollama-downloader.rb
+++ Formula/ollama-downloader.rb
@@ -1,13 +1,13 @@
 class OllamaDownloader < Formula
   include Language::Python::Virtualenv
 
-  deprecate! date: "2026-02-20", because: "will be eventually replaced by odir", replacement_formula: "odir"
-
   desc "Library and Hugging Face model downloader for Ollama"
   homepage "https://pypi.org/project/ollama-downloader/"
   url "https://github.com/anirbanbasu/ollama-downloader/archive/refs/tags/v.0.2.2.tar.gz"
   sha256 "4a82a5dc286624ef9baee2b42f4c340f4af3244116c14ba545fd931201fcdca9"
   license "MIT"
+
+  deprecate! date: "2026-02-20", because: "will be eventually replaced by odir", replacement_formula: "odir"
 
   depends_on "rust" => :build
   depends_on "python@3.14"
