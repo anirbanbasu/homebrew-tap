7aa15cb6ca37435f44e2d22a5445450ba9bd60bb
Anirban Basu <anirbanbasu@users.noreply.github.com>
2026-04-30 12:56:40 +0900

    chore: Update Bibtera to version 0.1.1
    Updated the version, URL, and SHA256 checksum for Bibtera.
    
    Signed-off-by: Anirban Basu <anirbanbasu@users.noreply.github.com>

--- Formula/bibtera.rb
+++ Formula/bibtera.rb
@@ -1,9 +1,8 @@
 class Bibtera < Formula
   desc "BibTeX translator using the Tera templating engine"
   homepage "https://github.com/anirbanbasu/bibtera"
-  # version "0.1.0"
-  url "https://github.com/anirbanbasu/bibtera/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "667af4dec3da7fcbce9fc0c1b04118932dc4ca87ad76616dc9d94bb8e5d4aec5"
+  url "https://github.com/anirbanbasu/bibtera/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "212d4d71adad18c10eae74f1c90ee76e28429841554f41be4bad8b27976b8020"
   license "MIT"
 
   depends_on "rust" => :build
