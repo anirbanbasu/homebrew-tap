780bd8401578e36df420db6acfeee048f2ab4265
Anirban Basu <anirbanbasu@users.noreply.github.com>
2026-09-09 15:12:29 +0900

    chore: Update ODIR to version 0.2.0
    Signed-off-by: Anirban Basu <anirbanbasu@users.noreply.github.com>

--- Formula/odir.rb
+++ Formula/odir.rb
@@ -1,9 +1,9 @@
 class Odir < Formula
   desc "Ollama Downloader in Rust (ODIR): a model downloader for Ollama"
   homepage "https://github.com/anirbanbasu/odir"
-  # version "0.1.1"
-  url "https://github.com/anirbanbasu/odir/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "fbae94d21d152da6c7e4a72b39c65b321a4ae35b57d69161986cce5ba86e4749"
+  # version "0.2.0"
+  url "https://github.com/anirbanbasu/odir/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "ed877f9e5f1c84063c4f22f5e0d88e3e78f35c47403dfd3d76d191ff7afbd57d"
   license "MIT"
 
   depends_on "rust" => :build
